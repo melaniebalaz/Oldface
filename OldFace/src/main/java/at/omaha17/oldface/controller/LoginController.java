74ea3bf4dad8846575e1f348d396415609821511
Melanie Balaz <melanie.j.balaz@gmail.com>
2016-11-28 17:25:21 +0100

    moved the Readme

--- OldFace/src/main/java/at/omaha17/oldface/controller/LoginController.java
+++ OldFace/src/main/java/at/omaha17/oldface/controller/LoginController.java
@@ -15,22 +15,17 @@ import at.omaha17.oldface.logic.UserManager;
  */
 @WebServlet("/index")
 public class LoginController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public LoginController() {
-        super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -38,14 +33,6 @@ public class LoginController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		
-		UserManager usrMgmt = new UserManager();
-		User user = usrMgmt.authenticate(username, password);
-		
-		
-		doGet(request, response);
 	}
 
 }
--- OldFace/src/main/java/at/omaha17/oldface/dao/UserDAOImpl.java
+++ OldFace/src/main/java/at/omaha17/oldface/dao/UserDAOImpl.java
@@ -5,7 +5,6 @@ import at.omaha17.oldface.model.User;
 
 public class UserDAOImpl implements UserDAO {
 
-	@Override
 	public List<User> getUserList() {
 		// TODO Auto-generated method stub
 		return null;
