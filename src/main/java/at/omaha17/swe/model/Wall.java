d161a9a7f1bbbafa5a9684068caa42504ed140bc
Stefan Berg <a1425065@unet.univie.ac.at>
2016-12-01 11:55:21 +0100

    Senior and Wll changes

--- src/main/java/at/omaha17/swe/model/Wall.java
+++ src/main/java/at/omaha17/swe/model/Wall.java
@@ -1,17 +1,38 @@
 package at.omaha17.swe.model;
 
 import java.util.Vector;
-import at.omaha17.swe.model.*;
-public class Wall {
-	private Senior senior;
-    Vector<Post> posts;
+import at.omaha17.swe.model.Post;
+import at.omaha17.swe.model.Senior;
 
+<<<<<<< HEAD
 	public Wall(Vector<Post> posts) {
 		
 		this.posts = posts;
+=======
+public class Wall {
+	
+	private Senior senior;
+    
+	Vector<Post> posts;
+
+	//Kann es sein dass der Wall bei der Kreation leer ist, da er mit einem neuem User kreiert wird 
+	//(wir erst später mit posts befüllt?
+	
+	public Wall() {
+>>>>>>> branch 'master' of https://gitta-lab.cs.univie.ac.at/swe16/g6t2.git
 		
 	}
 
+<<<<<<< HEAD
 	
+=======
+	public Vector<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(Vector<Post> posts) {
+		this.posts = posts;
+	}
+>>>>>>> branch 'master' of https://gitta-lab.cs.univie.ac.at/swe16/g6t2.git
     
 }
