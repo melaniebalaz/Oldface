ca3021035bb5ffee5718ff76335e26007344f765
Stefan Berg <a1425065@unet.univie.ac.at>
2016-12-01 10:54:16 +0100

    Model

--- src/main/java/at/omaha17/swe/model/User.java
+++ src/main/java/at/omaha17/swe/model/User.java
@@ -1,25 +1,30 @@
 package at.omaha17.swe.model;
 
-import java.io.Serializable;
 
+import java.io.Serializable;
+import java.util.Calendar;
 import java.util.Vector;
 
 public class User {
-    public final static String ROLE_RETIREE = "Retiree";
+    public final static String ROLE_SENIOR = "Senior";
     public final static String ROLE_ADMIN = "Admin";
     public final static String ROLE_RESEARCHER = "Researcher";
 
-	private String role = ROLE_RETIREE;
+	private String role = ROLE_SENIOR;
     private String email;
     private String password;
-    private String displayName;
+    //private String displayName;
+    private Calendar creation_date;
+    private Calendar login_date;
     private Vector<String> authenticationTokens;
 
 
-    public User(String role, String email, String password,String displayName) {
+    public User(String role, String email, String password, Calendar creation_date, Calendar login_date) {
         this.role = role;
         this.email = email;
         this.password = password;
+        this.creation_date = creation_date;
+        this.login_date = login_date;
     }
 
     public String getRole() {
@@ -33,8 +38,26 @@ public class User {
     public String getPassword() {
         return password;
     }
+    
+    public void setCreation_date(Calendar creation_date) {
+    	Calendar jetzt = Calendar.getInstance();
+		this.creation_date = jetzt;
+	}
+    
+    public Calendar getCreation_date() {
+    	return creation_date;
+	}
 
-    public String getDisplayName() {
+	public void setLogin_date(Calendar login_date) {
+		Calendar jetzt = Calendar.getInstance();
+		this.creation_date = jetzt;
+	}
+
+	public Calendar getLogin_date() {
+		return login_date;
+	}
+	
+    /*public String getDisplayName() {
         return displayName;
-    }
+    }*/
 }
