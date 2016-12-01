1355d25b6fb52d2b6b30f305981fc1cb43241e70
Stefan Berg <a1425065@unet.univie.ac.at>
2016-12-01 11:09:19 +0100

    wall

--- src/main/java/at/omaha17/swe/model/Wall.java
+++ src/main/java/at/omaha17/swe/model/Wall.java
@@ -5,4 +5,14 @@ import java.util.Vector;
 public class Wall {
 
     Vector<Post> posts;
+
+	public Wall(Vector<Post> posts) {
+		super();
+		this.posts = posts;
+	}
+
+	/*public Wall() {
+		super();
+	} falls ohne Vector*/
+    
 }
