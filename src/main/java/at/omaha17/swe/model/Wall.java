97d303345ec91a69a679a62f68a712e6233ca559
Stefan Berg <a1425065@unet.univie.ac.at>
2016-12-01 11:15:27 +0100

    wall

--- src/main/java/at/omaha17/swe/model/Wall.java
+++ src/main/java/at/omaha17/swe/model/Wall.java
@@ -1,14 +1,15 @@
 package at.omaha17.swe.model;
 
 import java.util.Vector;
-
+import at.omaha17.swe.model.*;
 public class Wall {
-
+	private Senior senior;
     Vector<Post> posts;
 
 	public Wall(Vector<Post> posts) {
 		super();
 		this.posts = posts;
+		this.senior = new Senior();
 	}
 
 	/*public Wall() {
