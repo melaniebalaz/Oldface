f9f9ff88d10882fc1abeaf07eafbcf606e90ef3a
Hannes Kramml <a1503268@unet.univie.ac.at>
2016-12-06 23:26:32 +0100

    implemented methods saveUser, deleteUser & getUserList within UserDAOSerialization

--- src/main/java/at/omaha17/swe/dao/UserDAOSerialization.java
+++ src/main/java/at/omaha17/swe/dao/UserDAOSerialization.java
@@ -2,10 +2,7 @@ package at.omaha17.swe.dao;
 
 import at.omaha17.swe.model.User;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.util.Vector;
 
 public class UserDAOSerialization implements UserDAO {
@@ -16,17 +13,55 @@ public class UserDAOSerialization implements UserDAO {
         this.filename = filename;
     }
 
+    @SuppressWarnings("unchecked")
     public void saveUser(User user) {
+        Vector<User> userList;
+        ObjectInputStream objectInputStream;
+        ObjectOutputStream objectOutputStream;
 
+        try {
+            objectInputStream = new ObjectInputStream(new FileInputStream(filename));
+            userList = (Vector<User>) objectInputStream.readObject();
+            objectInputStream.close();
+
+            for (User usr : userList)
+                if (usr.getUsername().equals(user.getUsername())) userList.remove(usr);
+
+            userList.add(user);
+
+            objectOutputStream = new ObjectOutputStream(new FileOutputStream(filename));
+            objectOutputStream.writeObject(userList);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+
+        } catch (IOException|ClassNotFoundException e) { e.printStackTrace(); }
     }
 
+    @SuppressWarnings("unchecked")
     public void deleteUser(User user) throws UserNotFoundException {
+        Vector<User> userList;
+        ObjectInputStream objectInputStream;
+        ObjectOutputStream objectOutputStream;
 
+        try {
+            objectInputStream = new ObjectInputStream(new FileInputStream(filename));
+            userList = (Vector<User>) objectInputStream.readObject();
+            objectInputStream.close();
+
+            if (userList.contains(user)) userList.remove(user);
+            else throw new UserNotFoundException();
+
+            objectOutputStream = new ObjectOutputStream(new FileOutputStream(filename));
+            objectOutputStream.writeObject(userList);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+
+        } catch (IOException|ClassNotFoundException e) { e.printStackTrace(); }
     }
 
     @SuppressWarnings("unchecked")
     public User getUserByUsername(String username) throws UserNotFoundException {
-        Vector<User> userList = null;
+        Vector<User> userList;
         ObjectInputStream objectInputStream;
 
         try {
@@ -34,21 +69,29 @@ public class UserDAOSerialization implements UserDAO {
             userList = (Vector<User>) objectInputStream.readObject();
             objectInputStream.close();
 
-        } catch (FileNotFoundException e) { e.printStackTrace(); }
-        catch (IOException e) { e.printStackTrace(); }
-        catch (ClassNotFoundException e) {
-            System.out.println("Class not found: User");
-            e.printStackTrace();
-        } catch (Exception e) { e.printStackTrace(); }
+            for (User user : userList)
+                if (user.getUsername().equals(username)) return user;
 
-        for (User user : userList)
-            if (user.getUsername().equals(username)) return user;
+            throw new UserNotFoundException();
 
-        throw new UserNotFoundException();
+        } catch (IOException|ClassNotFoundException e) { e.printStackTrace(); }
+
+        return null;
     }
 
+    @SuppressWarnings("unchecked")
     public Vector<User> getUserList() {
-        return null;
+        Vector<User> userList;
+        ObjectInputStream objectInputStream;
+
+        try {
+            objectInputStream = new ObjectInputStream(new FileInputStream(filename));
+            userList = (Vector<User>) objectInputStream.readObject();
+            objectInputStream.close();
+
+        } catch (IOException|ClassNotFoundException e) { e.printStackTrace(); return null; }
+
+        return userList;
     }
 
 }
