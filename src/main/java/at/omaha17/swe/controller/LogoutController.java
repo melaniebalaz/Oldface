1266991a5e33de6661479a17bd1980606de25e0a
Melanie Balaz <melanie.j.balaz@gmail.com>
2016-12-07 11:09:17 +0100

    controller structure is getting there

---
+++ src/main/java/at/omaha17/swe/controller/LogoutController.java
@@ -0,0 +1,26 @@
+package at.omaha17.swe.controller;
+
+import at.omaha17.swe.logic.WallManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/wall")
+public class LogoutController extends HttpServlet {
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session=request.getSession();
+        session.invalidate();
+
+        response.sendRedirect("/login");
+
+    }
+}
--- src/main/java/at/omaha17/swe/controller/RegisterController.java
+++ src/main/java/at/omaha17/swe/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package at.omaha17.swe.controller;
 
+import at.omaha17.swe.logic.RegistrationFailedException;
 import at.omaha17.swe.logic.UserManager;
 
 import javax.servlet.ServletException;
@@ -7,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import org.jtwig.web.servlet.JtwigRenderer;
@@ -44,16 +46,16 @@ public class RegisterController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String name = request.getParameter("name");
-        String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        manager.registerUser()
-
-        //Now sent the user details to the user manager with the method call of register user
-        //Hope that this call returns me a authentification token
-        //Wrap the whole thing in a try catch block, if I get an exception render the error page
+        try {
+            manager.registerUser("ROLE_SENIOR", name, password);
+        } catch (RegistrationFailedException exception){
+            response.sendRedirect("/registerError");
+        }
 
         //redirect to the wall page
+        HttpSession session=request.getSession();
         response.sendRedirect("/wall");
 
     }
