98435a8b6873d3ecd593fc2655a484697288cff4
Melanie Balaz <melanie.j.balaz@gmail.com>
2016-12-07 17:31:08 +0100

    fixed the folder strucutre, deploying works now locally with the tomcat server and maven

--- src/main/java/at/omaha17/swe/controller/LoginController.java
+++ src/main/java/at/omaha17/swe/controller/LoginController.java
@@ -40,7 +40,7 @@ public class LoginController extends HttpServlet {
 	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        renderer.dispatcherFor("/WEB-INF/templates/external/login.twig.html")
+        renderer.dispatcherFor("/WEB-INF/templates/external/login.twig")
                 .render(request,response);
 
 	}
