f24ee1207af34b3bf4d1ae10a4dab76cce689ae4
Melanie Balaz <melanie.j.balaz@gmail.com>
2016-12-12 15:04:18 +0100

    some small frontend changes, everything should be implemented correctly now, but somehow the comments dont quite work yet

--- src/main/java/at/omaha17/swe/controller/WallCommentController.java
+++ src/main/java/at/omaha17/swe/controller/WallCommentController.java
@@ -1,15 +1,13 @@
 package at.omaha17.swe.controller;
 
-import at.omaha17.swe.logic.UserManager;
-import at.omaha17.swe.logic.UserManagerImpl;
-import at.omaha17.swe.logic.WallManager;
-import at.omaha17.swe.logic.WallManagerImpl;
+import at.omaha17.swe.logic.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/comment")
@@ -19,10 +17,20 @@ public class WallCommentController extends HttpServlet {
     UserManager userManager = new UserManagerImpl();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String postContent = request.getParameter("comment");
+        String comment = request.getParameter("comment");
         String postID = request.getParameter("postID");
 
-        //wallManager.addComment(Post post, Senior author, String postContent);
+        HttpSession session=request.getSession();
+        String userName = (String) session.getAttribute("userName");
+
+        try {
+            wallManager.addComment(postID, userName, comment);
+            //Need to make a different redirect if a comment has been added on the dashboard..two controllers?
+            response.sendRedirect("/wall");
+        }catch(WallException exception){
+            //redirect to Comment error page
+        }
+
 
 
     }
