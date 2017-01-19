0552369b444f71e5f99b510757fc769be8c2f75b
Melanie Balaz <melanie.j.balaz@gmail.com>
2017-01-19 15:18:15 +0100

    added the new controller for the admin functionality

---
+++ src/main/java/at/omaha17/swe/controller/AdminFunctionalityController.java
@@ -0,0 +1,27 @@
+package at.omaha17.swe.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AdminFunctionalityController extends HttpServlet {
+
+    /**
+     * POST Request for deleting a post
+     * @param request
+     * @param response
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    /**
+     * GET Request for blocking a User
+     * @param request
+     * @param response
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
