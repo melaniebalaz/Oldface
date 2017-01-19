c194e10836bde333d589ec40ef456b57112f8a95
Hannes Kramml <a1503268@unet.univie.ac.at>
2017-01-19 20:17:01 +0100

    fixed dashboard zero messages issue, implemented sort logic for wall

--- src/main/java/at/omaha17/swe/logic/VisualizationManager.java
+++ src/main/java/at/omaha17/swe/logic/VisualizationManager.java
@@ -5,7 +5,6 @@ import at.omaha17.swe.dao.MessageDAOSerialization;
 import at.omaha17.swe.dao.UserDAO;
 import at.omaha17.swe.dao.UserDAOSerialization;
 import at.omaha17.swe.model.*;
-import javafx.geometry.Pos;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -15,12 +14,14 @@ public class VisualizationManager {
 
     private static final UserDAO userDAO = new UserDAOSerialization("UserDB.ser");
     private static final MessageDAO messageDAO = new MessageDAOSerialization("MessageDB.ser");
-    private static final int postCount = 10;
+    private static final int postDisplayCount = 10;
 
     public static Wall getWall(String username) throws TechnicalException {
 
         try {
-            return new Wall((Senior) userDAO.getUserByUsername(username), messageDAO.getPostsByUsername(username));
+            Vector<Post> posts = messageDAO.getPostsByUsername(username);
+            if (posts != null) posts.sort(Collections.reverseOrder());
+            return new Wall((Senior) userDAO.getUserByUsername(username), posts);
 
         } catch (IOException|ClassNotFoundException|IllegalArgumentException e) { throw new TechnicalException(e); }
     }
@@ -30,7 +31,8 @@ public class VisualizationManager {
         try {
             User user = userDAO.getUserByUsername(username);
             Vector<Post> allPosts = messageDAO.getPostList();
-            allPosts.sort(Collections.reverseOrder());
+            if (allPosts == null) return new Dashboard(user);
+            Dashboard dashboard = new Dashboard(user);
 
             if (user instanceof Senior) {
                 Vector<Post> followerPosts = new Vector<>();
@@ -40,35 +42,39 @@ public class VisualizationManager {
                     followerPosts.add(getRecentUserPost(follower.getUsername()));
 
                 followerPosts.sort(Collections.reverseOrder());
-                Dashboard dashboard = new Dashboard(user);
 
                 int index = 0;
-                while (index < followerPosts.size() && dashboard.getPosts().size() < postCount) {
+                while (index < followerPosts.size() && dashboard.getPosts().size() < postDisplayCount) {
                     dashboard.addPost(followerPosts.get(index));
                     index++;
                 }
+            }
 
-                index = 0;
-                while (index < allPosts.size() && dashboard.getPosts().size() < postCount) {
-                    if (!dashboard.getPosts().contains(allPosts.get(index))) dashboard.addPost(allPosts.get(index));
-                    index++;
-                }
+            // fill up with other posts until postCount is reached
+            allPosts.sort(Collections.reverseOrder());
+            int index = 0;
+            while (index < allPosts.size() && dashboard.getPosts().size() < postDisplayCount) {
+                if (!dashboard.getPosts().contains(allPosts.get(index))) dashboard.addPost(allPosts.get(index));
+                index++;
+            }
 
-                return dashboard;
-
-            } else
-                return new Dashboard(user, allPosts);
+            return dashboard;
 
         } catch (Exception e) { throw new TechnicalException(e);}
     }
 
-    public static void getStatistic() {
+    public static Statistic getStatistic() throws TechnicalException {
 
+        try {
+            return null;
+
+        } catch (Exception e) { throw new TechnicalException(e);}
     }
 
     private static Post getRecentUserPost(String username) throws IOException, ClassNotFoundException {
 
         Vector<Post> posts = messageDAO.getPostsByUsername(username);
+        if (posts == null) return null;
         Collections.sort(posts);
         return posts.get(posts.size() - 1);
     }
