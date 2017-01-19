c9c47462b7f4d19d5d3e8d542dc90f880a8dfa9e
Melanie Balaz <melanie.j.balaz@gmail.com>
2017-01-19 15:39:52 +0100

    NEW: Statistic class in the Modal for passing around Statistics

---
+++ src/main/java/at/omaha17/swe/model/Statistic.java
@@ -0,0 +1,69 @@
+package at.omaha17.swe.model;
+
+public class Statistic {
+
+    private String userCount;
+    private int numberOfPosts;
+    private int postsPerDay;
+    private int postLength;
+    private int numberOfCommentsPerPost;
+    private int commentsPerDay;
+    private int commentLength;
+
+    public String getUserCount() {
+        return userCount;
+    }
+
+    public void setUserCount(String userCount) {
+        this.userCount = userCount;
+    }
+
+    public int getNumberOfPosts() {
+        return numberOfPosts;
+    }
+
+    public void setNumberOfPosts(int numberOfPosts) {
+        this.numberOfPosts = numberOfPosts;
+    }
+
+    public int getPostsPerDay() {
+        return postsPerDay;
+    }
+
+    public void setPostsPerDay(int postsPerDay) {
+        this.postsPerDay = postsPerDay;
+    }
+
+    public int getPostLength() {
+        return postLength;
+    }
+
+    public void setPostLength(int postLength) {
+        this.postLength = postLength;
+    }
+
+    public int getNumberOfCommentsPerPost() {
+        return numberOfCommentsPerPost;
+    }
+
+    public void setNumberOfCommentsPerPost(int numberOfCommentsPerPost) {
+        this.numberOfCommentsPerPost = numberOfCommentsPerPost;
+    }
+
+    public int getCommentsPerDay() {
+        return commentsPerDay;
+    }
+
+    public void setCommentsPerDay(int commentsPerDay) {
+        this.commentsPerDay = commentsPerDay;
+    }
+
+    public int getCommentLength() {
+        return commentLength;
+    }
+
+    public void setCommentLength(int commentLength) {
+        this.commentLength = commentLength;
+    }
+
+}
