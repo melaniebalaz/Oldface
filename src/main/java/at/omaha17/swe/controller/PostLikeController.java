10e9991aa702574e818664f240bb8301d90bef4e
Melanie Balaz <melanie.j.balaz@gmail.com>
2017-01-21 17:56:18 +0100

    fixed url for controller

--- src/main/java/at/omaha17/swe/controller/PostLikeController.java
+++ src/main/java/at/omaha17/swe/controller/PostLikeController.java
@@ -5,12 +5,15 @@ import at.omaha17.swe.logic.TechnicalException;
 import org.jtwig.web.servlet.JtwigRenderer;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLEncoder;
 
+
+@WebServlet("/like")
 public class PostLikeController extends HttpServlet {
 
 
