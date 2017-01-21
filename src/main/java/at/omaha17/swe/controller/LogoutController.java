e88ec8209262a43bd53a991cef9efd4d15b80a91
Melanie Balaz <melanie.j.balaz@gmail.com>
2017-01-21 23:11:05 +0100

    a few layouting changes

--- src/main/java/at/omaha17/swe/controller/LogoutController.java
+++ src/main/java/at/omaha17/swe/controller/LogoutController.java
@@ -15,7 +15,7 @@ public class LogoutController extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        HttpSession session=request.getSession();
+        HttpSession session=request.getSession(false);
         session.invalidate();
 
         response.sendRedirect("/");
